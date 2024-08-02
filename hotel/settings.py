b0b9873d33fc9376f4202f2601c4ad6e7ca881d5
adamu <adams.square@yahoo.com>
2024-08-02 12:53:17 +0300

    debug to true intentionally to see if style works

--- hotel/settings.py
+++ hotel/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-y0tjvd@thr%0jb6uuoqxt4i=yl-^tp$(nfupx3d+z(3$a+w(ky"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "hotelsite-7y00.onrender.com"]
--- hotel/urls.py
+++ hotel/urls.py
@@ -1,5 +1,3 @@
-
-
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -19,13 +17,6 @@ urlpatterns = [
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-
-# Serve static files during development
 if settings.DEBUG:
-    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
