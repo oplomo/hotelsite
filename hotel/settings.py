218c5d1811391f2b0ed8dd0b89e8878a32d50bed
adamu <adams.square@yahoo.com>
2024-08-02 11:22:48 +0300

    Update ALLOWED_HOSTS for Render deployment

--- hotel/settings.py
+++ hotel/settings.py
@@ -24,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-y0tjvd@thr%0jb6uuoqxt4i=yl-^tp$(nfupx3d+z(3$a+w(ky"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'hotelsite-7y00.onrender.com']
+
 
 
 # Application definition
